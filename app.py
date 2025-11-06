c7f575bcaa8970a2177d3a0bf6eb0151bed9fea2
Joshua Jones <184120456+JoshTheC0der@users.noreply.github.com>
2025-11-06 08:12:40 +0000

    Change greeting message in app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 
 def greetings():
-    return "<p> Greetings Young Pimp! </p>"
+    return "<p> Greetings Young Padawon! </p>"
 
 if __name__ == "__main__":
   app.run()
